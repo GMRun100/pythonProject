e4e87fb03bf36abf2935a9edd1aea9553efcae82
GMRun100 <734122618@qq.com>
2019-02-08 16:24:00 +0100

    add list_tuple.py

--- hello_world/hello.py
+++ hello_world/hello.py
@@ -1,9 +1,21 @@
+#！/usr/bin/env python3
+# -*- coding: utf-8 -*-
 #print('hello world!')
 #name=input('please enter your name:')
 #print('hello,',name)
 #print('''line1
 #line2
 #line3''')
-print(r'''line1
-line2
-line3''')
+#print(r'''line1
+#line2
+#line3''')
+
+#print('中文测试')
+
+#第一种格式化字符串的方法
+#前面加0当位数不够时可以补0，
+#print('%2d-%05d' % (3000,1))
+#print('%.5f' % 3.1) 
+
+#第二种格式化字符串的方法，采用format,用这种方法比用%方法要麻烦
+print('Hello,{0},成绩提升了{1:.1f}%,{2}'.format('小明',17.125,'haha'))
